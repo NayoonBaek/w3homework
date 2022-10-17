3c2401051815fff46ad54539f20a011b15f3f33c
NayoonBaek <vlfdyd57@naver.com>
2022-10-17 23:37:51 +0900

    세션에서 했던 로그인 구현

---
+++ src/main/java/com/sparta/w3homework/W3homeworkApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.w3homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W3homeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W3homeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/w3homework/W3homeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.w3homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W3homeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
